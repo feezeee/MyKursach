f7eb5ed3778f7e64a4881f335b73b55c292b9a81
Denis <45011033+feezeee@users.noreply.github.com>
2021-10-24 20:02:00 +0300

    Добавьте файлы проекта.

---
+++ MyKursach2/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyKursach.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyKursach.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Positions()
+        {
+            return View();
+        }
+
+        public ViewResult Workers()
+        {
+            return View();
+        }
+
+        public ViewResult Operations()
+        {
+            return View();
+        }
+    }
+}
