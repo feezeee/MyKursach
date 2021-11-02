4ba2d945d5925c1c9cdf22e9a7706cde8986ccfc
Denis <45011033+feezeee@users.noreply.github.com>
2021-11-03 02:49:50 +0300

    Action List was added

--- MyKursach2/Controllers/GenderController.cs
+++ MyKursach2/Controllers/GenderController.cs
@@ -17,5 +17,28 @@ namespace MyKursach2.Controllers
             _context = context;
         }
 
+
+        [Authorize(Roles = "Директор, Администратор")]
+        public ViewResult List(Gender gender)
+        {
+            //var res = _context.Workers.Join(_context.Positions);
+
+            var res = from gend in _context.Gender
+                      select new Gender
+                      {
+                          Id = gend.Id,
+                          GenderName = gend.GenderName
+                      };
+
+            if (gender?.Id > 0)
+            {
+                res = res.Where(i => i.Id == gender.Id).Select(i => i);
+            }
+            if (gender?.GenderName != null)
+            {
+                res = res.Where(fn => fn.GenderName.ToUpper().Contains(gender.GenderName.ToUpper())).Select(fn => fn);
+            }
+            return View(res);
+        }
     }
 }
