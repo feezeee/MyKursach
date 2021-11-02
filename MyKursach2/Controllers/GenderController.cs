248fb4519619d7e430450312797c9d493e664492
Denis <45011033+feezeee@users.noreply.github.com>
2021-11-03 02:49:36 +0300

    GenderController was init

---
+++ MyKursach2/Controllers/GenderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyKursach2.Data;
+using MyKursach2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyKursach2.Controllers
+{
+    public class GenderController : Controller
+    {
+        private ApplicationDbContext _context;
+        public GenderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
