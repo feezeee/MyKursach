c7ffd4f32903512fde45f4d680f9fabb25b7c60a
Денис Скурат <den.skur2016@yandex.ru>
2021-11-05 16:22:27 +0300

    Gender was developed

--- MyKursach2/Models/GenderModel/Gender.cs
+++ MyKursach2/Models/GenderModel/Gender.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,13 @@ namespace MyKursach2.Models
 {
     public class Gender
     {
+        [Required]
         public int Id { get; set; }
 
+
+        [Required(ErrorMessage = "Не указан номер телефона")]
+        [StringLength(19, ErrorMessage = "Длина строки должна быть до 19 символов")]
+        [Remote(action: "CheckGenderName", controller: "Gender", AdditionalFields = "Id", ErrorMessage = "Такой пол уже используется", HttpMethod = "POST")]
         public string GenderName { get; set; }
     }
 }
